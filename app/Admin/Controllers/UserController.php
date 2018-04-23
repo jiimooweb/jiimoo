51e9d3f9a8b69a8a14ec216461e3375ae8657fd5
jiimooyou <296738692@qq.com>
2018-04-23 14:47:13 +0800

    浩

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -9,70 +9,48 @@
 namespace App\Admin\Controllers;
 
 
-use App\Http\Controllers\Controller;
+use App\Admin\Controllers\Controller;
 use App\Models\Commons\AdminUser;
 class UserController extends Controller
 {
-        // 只出现用户
-        public function userLits(){
-            $users=AdminUser::where('identity','User')->paginate(15);
-            dd($users);
-        }
-        //用户自行修改自己信息
-        public function userUpdate(){
-            $this->validate(request(),[
-                'password'=>'required',
-            ]);
-            $password=request('password');
-            $update=AdminUser::where('id',\Auth::id())->update(['password'=>$password]);
-            return $update;
-        }
-        //添加用户
-        public function register(){
-            $this->validate(request(),[
-                'username'=>'required|unique:communal_users,username',
-                'email'=>'required|email|unique:communal_users,email',
-                'password'=>'required',
-            ]);
-            $username=request('username');
-            $email=	request('email');
-            $password=bcrypt(request('password'));
-            $status='Y';
-            $identity='User';
-            $user=AdminUser::create(compact('username','password','email','identity','status'));
-            dd($user);
-        }
-        public function registerIndex(){
-            return view('admin/register/index');
-        }
-        //登录
-        public function loginIndex(){
-            return view('admin/login/index');
-        }
-        public function login(){
-            $this->validate(request(),[
-                'account'=>'required',
-                'password'=>'required',
-            ]);
-            $username=request('account');
-            $email=	request('account');
-            $password=request('password');
-            $userNameAttempt=compact('username','password');
-            $userEmailAttempt=compact('email','password');
-            if(\Auth::guard('admins')->attempt($userNameAttempt)||
-               \Auth::guard('admins')->attempt($userEmailAttempt)){
-                return "登陆成功";
-            }
-            return "用户或密码错误";
-        }
-        public function logout(){
-            \Auth::guard('admin')->logout();
-            return "登出";
-        }
-
-        //删除用户
-        public function deleteUser(AdminUser $adminUser){
-            $delete=AdminUser::destroy($adminUser->id);
-            return "删除";
-        }
+    // 只出现用户
+    public function index(){
+        $users=AdminUser::where('identity','User')->paginate(15);
+        dd($users);
+    }
+    public function edit(AdminUser $adminUser){
+        return view('',compact('adminUser'));
+    }
+    //用户自行修改自己信息
+    public function update(){
+        $this->validate(request(),[
+            'password'=>'required',
+        ]);
+        $password=request('password');
+        $update=AdminUser::where('id',\Auth::id())->update(['password'=>$password]);
+        return $update;
+    }
+    //添加用户
+    public function store(){
+        $this->validate(request(),[
+            'username'=>'required|unique:admin_users,username',
+            'email'=>'required|email|unique:admin_users,email',
+            'password'=>'required',
+        ]);
+        $username=request('username');
+        $email=	request('email');
+        $password=bcrypt(request('password'));
+        $status='Y';
+        $identity='User';
+        $user=AdminUser::create(compact('username','password','email','identity','status'));
+        dd($user);
+    }
+    public function create(){
+        return view('admin/register/index');
+    }
+    //删除用户
+    public function delete(AdminUser $adminUser){
+        $delete=AdminUser::destroy($adminUser->id);
+        return "删除";
+    }
 }
