d61ddf7a134037559d90b3920bca1db979766c9e
FanWaterWater <598804008@qq.com>
2018-04-27 17:47:44 +0800

    commit

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -17,7 +17,7 @@ class TokenController extends Controller
 
         $token = $client->getToken(request('username'), request('password'));
 
-        if( is_string($str) && strlen($token) === 32) {
+        if( is_string($token) && strlen($token) === 32) {
             return response()->json(['token' => $token, 'msg' => '登录成功！']);
         }
         
