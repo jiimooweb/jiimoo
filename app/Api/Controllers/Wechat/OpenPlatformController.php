bbef43ce2efd82ada1193e1192b3098dd2d8705a
FanWaterWater <598804008@qq.com>
2018-05-20 20:24:56 +0800

    commit

--- app/Api/Controllers/Wechat/OpenPlatformController.php
+++ app/Api/Controllers/Wechat/OpenPlatformController.php
@@ -62,7 +62,7 @@ class OpenPlatformController extends Controller
     public function user_authorize() 
     {
         $openPlatform = OpenPlatform::getApp();
-        $url = $openPlatform->getPreAuthorizationUrl('http://rdoorweb.com');
+        $url = $openPlatform->getPreAuthorizationUrl('http://www.rdoorweb.com/wechat/authorize');
         return redirect($url);
     }
 
