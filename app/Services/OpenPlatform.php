b4ed2d0b4e17a39f60e61bd8b00ca4936c542b5e
FanWaterWater <598804008@qq.com>
2018-05-21 22:46:49 +0800

    commit

--- app/Services/OpenPlatform.php
+++ app/Services/OpenPlatform.php
@@ -102,8 +102,7 @@ class OpenPlatform
         $data['app_id'] = $miniProgram['authorization_info']['authorizer_appid'];         
         $data['refresh_token'] = $miniProgram['authorization_info']['authorizer_refresh_token'];
         $data['func_info'] = serialize($miniProgram['authorization_info']['func_info']); 
-        dd($authorizer_info);
         // return Xcx::where('xcx_id', session('xcx_id'))->update($data) ? true : false;
-        return Xcx::where('id', 33)->update($authorizer_info) ? true : false;
+        return Xcx::where('id', 33)->update($data) ? true : false;
     }
 }
