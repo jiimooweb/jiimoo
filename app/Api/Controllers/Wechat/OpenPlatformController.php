31c2adf1ead158e543ed71e954a05ab10773464e
FanWaterWater <598804008@qq.com>
2018-05-23 20:08:52 +0800

    commit

--- app/Api/Controllers/Wechat/OpenPlatformController.php
+++ app/Api/Controllers/Wechat/OpenPlatformController.php
@@ -105,7 +105,7 @@ class OpenPlatformController extends Controller
         
     }
 
-    public function callback($app_id)
+    public function receiver($app_id)
     {
         // $official = $this->initOfficialAccount();
         $openPlatform = OpenPlatform::getApp();
@@ -115,20 +115,30 @@ class OpenPlatformController extends Controller
 
         $msg = $server->getMessage();
 
-        $miniProgram = OpenPlatform::getMiniProgram();
-
         if ($msg['MsgType'] == 'text') {
             if ($msg['Content'] == 'TESTCOMPONENT_MSG_TYPE_TEXT') {
+                $miniProgram = $openPlatform->miniProgram($app_id, Cache::get($app_id));
                 $miniProgram->customer_service->message($msg['Content'] . '_callback')
                     ->from($msg['ToUserName'])->to($msg['FromUserName'])->send();
                 die;
             } elseif (strpos($msg['Content'], 'QUERY_AUTH_CODE') == 0) {
                 echo '';
                 $code           = substr($msg['Content'], 16);
+                $authorizerInfo = $openPlatform->handleAuthorize($code)['authorization_info'];
+                Cache::put(
+                    $authorizerInfo['authorizer_appid'], 
+                    $authorizerInfo['authorizer_refresh_token'],
+                    7200
+                );
+                $miniProgram = $openPlatform->miniProgram(
+                    $authorizerInfo['authorizer_appid'], 
+                    $authorizerInfo['authorizer_refresh_token']
+                );
                 $miniProgram->customer_service->message($code . "_from_api")
                             ->from($msg['ToUserName'])->to($msg['FromUserName'])->send();
             }
         } elseif ($msg['MsgType'] == 'event') {
+            $miniProgram = $openPlatform->miniProgram($app_id, Cache::get($app_id));
             $miniProgram->customer_service->message($msg['Event'] . 'from_callback')
                 ->to($msg['FromUserName'])->from($msg['ToUserName'])->send();
             die;
