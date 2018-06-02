9c7cecc55ebe7e18b496930db83ea605f5739254
FanWaterWater <598804008@qq.com>
2018-06-02 14:35:29 +0800

    commit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,7 +20,7 @@ class Cors {
     $response->header('Access-Control-Allow-Origin', '*');
     $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
     $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS,X-CSRF-TOKEN');
-    // $response->header('Access-Control-Allow-Credentials', 'true');
+    $response->header('Access-Control-Allow-Credentials', 'true');
     return $response;
   }
 
