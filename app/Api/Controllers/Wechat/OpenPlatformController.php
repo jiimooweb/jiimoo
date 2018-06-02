04b9d346786276118afd71685a73a90876316e42
FanWaterWater <598804008@qq.com>
2018-06-02 14:01:30 +0800

    c

--- app/Api/Controllers/Wechat/OpenPlatformController.php
+++ app/Api/Controllers/Wechat/OpenPlatformController.php
@@ -139,7 +139,7 @@ class OpenPlatformController extends Controller
     {
         $miniProgram = OpenPlatform::getMiniProgram(request()->xcx_id);
         $file = $miniProgram->code->getQrCode('pages/index/index');
-        dd($file->toArray());
+        dd(json_decode($file));
         
         dd($file->headers);
         
