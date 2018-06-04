50e31d759feca95d1efc0a43d5a281072ce3c7a8
AinzOoalGown0420 <1844853576@qq.com>
2018-06-04 12:18:00 +0800

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link rel="shortcut icon" href="{{URL::asset('logo.png')}}"/>
+    <link rel="shortcut icon" href="{{URL::asset('favicon.ico')}}"/>
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
