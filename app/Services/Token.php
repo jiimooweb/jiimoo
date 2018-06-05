2a76912d90b02c52c27165146edc7b3ff7d024b8
FanWaterWater <598804008@qq.com>
2018-06-05 09:31:17 +0800

    commit

--- app/Services/Token.php
+++ app/Services/Token.php
@@ -154,7 +154,7 @@ class Token
      * @return bool
      * @throws Exception
      */
-    public static function isValidOperate(int $checkedUID) : boolean
+    public static function isValidOperate(int $checkedUID) : bool
     {
         if (!$checkedUID) {
             return response()->json(['msg' => '检查UID时必须传入一个被检查的UID']);
@@ -172,7 +172,7 @@ class Token
      * @param $token 
      * @return bool
      */
-    public static function verifyToken(string $token) : boolean
+    public static function verifyToken(string $token) : bool
     {
         $exist = Cache::get($token);
         if ($exist) {
