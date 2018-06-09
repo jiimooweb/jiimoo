20add9e25ad7c32a5040cddd564a4af33a2d76e6
FanWaterWater <598804008@qq.com>
2018-06-09 11:19:15 +0800

    commit

--- app/Api/Controllers/Commons/BasicInfoController.php
+++ app/Api/Controllers/Commons/BasicInfoController.php
@@ -43,9 +43,9 @@ class BasicInfoController extends Controller
     {
         // TODO:判断更新权限
         $data = request()->all();
-        dd($data);
-        $data['images'] = $data['images'] ? json_encode($data['images']) : null;        
 
+        $data['images'] = $data['images'] ? json_encode($data['images']) : null;        
+        dd($data['images']);
         if(BasicInfo::where('xcx_id', request()->info)->update($data)) {
             return response()->json(['status' => 'success', 'msg' => '更新成功！']);    
         }
