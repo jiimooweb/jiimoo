5b5c3b6dc89078e6076c522896e943154e78f07f
AinzOoalGown0420 <1844853576@qq.com>
2018-06-25 17:50:04 +0800

    hello vue

---
+++ resources/assets/js/components/hello/test1.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>test1</h1>
+        <el-button @click="visible = true">按钮</el-button>
+        <el-dialog v-model="visible" title="Hello world">
+          <p>欢迎使用 Element</p>
+        </el-dialog>
+        <group>
+          <cell title="title" value="value"></cell>
+        </group>
+    </div>
+</template>
+
+<script>
+    import { Group, Cell } from 'vux'
+    export default{
+        components: {
+            Group,
+            Cell
+        },
+        data(){
+            return {
+                visible: false
+            }
+        },
+        methods : {
+
+        },
+        mounted(){
+
+        }
+    }
+</script>
