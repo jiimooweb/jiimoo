8629727dbc31e6f471c4de6a84121e9a88ad6dc0
jimocc <245629490@qq.com>
2018-06-26 10:34:09 +0800

    修改判断

--- app/Http/Requests/Votes/VoteUpdateRequest.php
+++ app/Http/Requests/Votes/VoteUpdateRequest.php
@@ -32,11 +32,14 @@ class VoteUpdateRequest extends CommonRequest
             'limit' => "bail|required|integer|max:{$num}|min:1",
         ];
         if ($type == 0) {
-            $applyStartDate = $arr['apply_start_date'];
-            $rules['apply_start_date'] = "bail|required|before:{$voteDueDate}";
-            $rules['apply_due_date'] = "bail|required|after:{$applyStartDate}|before_or_equal:{$voteDueDate}";
-            $rules['is_check'] = 'required';
             $rules['is_apply'] = 'required';
+            $isApply = request('is_apply');
+            if($isApply == 1){
+                $applyStartDate = $arr['apply_start_date'];
+                $rules['apply_start_date'] = "bail|required|before:{$voteDueDate}";
+                $rules['apply_due_date'] = "bail|required|after:{$applyStartDate}|before_or_equal:{$voteDueDate}";
+            }
+            $rules['is_check'] = 'required';
         }
         return $rules;
     }
