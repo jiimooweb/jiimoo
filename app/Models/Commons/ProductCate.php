07ceb554a602ad084014390b4ed98f17b91e4664
FanWaterWater <598804008@qq.com>
2018-06-28 17:42:18 +0800

    commit

--- app/Models/Commons/ProductCate.php
+++ app/Models/Commons/ProductCate.php
@@ -13,7 +13,7 @@ class ProductCate extends Model
         return $this->hasMany(Product::class, 'cate_id', 'id')->orderBy('created_at','desc');
     }
 
-    public function getChildrens(int $pid) : ProductCate
+    public function getChildrens(int $pid)
     {
         return $this->where('pid', $pid)->get()->pluck('id')->toArray();
     }
