d5e7a5d36150c6f37a97d19574dd2e8a4ae95bb1
FanWaterWater <598804008@qq.com>
2018-06-30 11:22:23 +0800

    commit

--- app/Api/Controllers/Pay/FoodPayController.php
+++ app/Api/Controllers/Pay/FoodPayController.php
@@ -17,7 +17,9 @@ class FoodPayController extends Controller
         $app = $wechatPay->getApp();
 
         $response = $app->handlePaidNotify(function($message, $fail){
-            $order = Order::where('order_no', $message['out_trade_no'])->withoutGlobalScopes()->first();
+            \Log::info($message);
+            
+            $order = Order::where('order_nos', $message['out_trade_no'])->withoutGlobalScopes()->first();
             \Log::info($order);
         
             if (!$order || $order->pay_time) { // 如果订单不存在 或者 订单已经支付过了
