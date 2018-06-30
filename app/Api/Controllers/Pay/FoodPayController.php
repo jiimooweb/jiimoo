6783cfac35db677a63e39d153b5dbdb9c897f689
FanWaterWater <598804008@qq.com>
2018-06-30 10:42:47 +0800

    commit

--- app/Api/Controllers/Pay/FoodPayController.php
+++ app/Api/Controllers/Pay/FoodPayController.php
@@ -17,37 +17,37 @@ class FoodPayController extends Controller
         $wechatPay = new WechatPay(config('notify.wechat.foods'));
         
         $app = $wechatPay->getApp();
-        
-        \Log::info($app);
 
-        $response = $app->handlePaidNotify(function($message, $fail){
+        // \Log::info($app);
 
-            $order = Order::where('order_no', $message['out_trade_no'])->first();
+        // $response = $app->handlePaidNotify(function($message, $fail){
+
+        //     $order = Order::where('order_no', $message['out_trade_no'])->first();
         
-            if (!$order || $order->pay_time) { // 如果订单不存在 或者 订单已经支付过了
-                return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-            }
+        //     if (!$order || $order->pay_time) { // 如果订单不存在 或者 订单已经支付过了
+        //         return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+        //     }
         
-            if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
-                // 用户是否支付成功
-                if (array_get($message, 'result_code') === 'SUCCESS') {
-                    $order->pay_time = data('Y-m-d H:i:s', time()); // 更新支付时间为当前时间
-                    $order->trans_no = $message['transaction_id']; // 更新支付时间为当前时间
-                    $order->status = OrderStatus::PAID;
+        //     if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
+        //         // 用户是否支付成功
+        //         if (array_get($message, 'result_code') === 'SUCCESS') {
+        //             $order->pay_time = data('Y-m-d H:i:s', time()); // 更新支付时间为当前时间
+        //             $order->trans_no = $message['transaction_id']; // 更新支付时间为当前时间
+        //             $order->status = OrderStatus::PAID;
         
-                // 用户支付失败
-                } elseif (array_get($message, 'result_code') === 'FAIL') {
-                    $order->status = OrderStatus::UNPAID;
-                }
-            } else {
-                return $fail('通信失败，请稍后再通知我');
-            }
+        //         // 用户支付失败
+        //         } elseif (array_get($message, 'result_code') === 'FAIL') {
+        //             $order->status = OrderStatus::UNPAID;
+        //         }
+        //     } else {
+        //         return $fail('通信失败，请稍后再通知我');
+        //     }
         
-            $order->save(); // 保存订单
+        //     $order->save(); // 保存订单
         
-            return true; // 返回处理完成
-        });
+        //     return true; // 返回处理完成
+        // });
         
-        return $response;
+        return $app;
     }
 }
