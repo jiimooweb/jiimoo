ba23ed391062f815dae2f008b22485effd9bf2f6
FanWaterWater <598804008@qq.com>
2018-06-30 11:27:53 +0800

    commit

--- app/Api/Controllers/Pay/FoodPayController.php
+++ app/Api/Controllers/Pay/FoodPayController.php
@@ -17,9 +17,8 @@ class FoodPayController extends Controller
         $app = $wechatPay->getApp();
 
         $response = $app->handlePaidNotify(function($message, $fail){
-            \Log::info($message);
             
-            $order = Order::where('order_nos', $message['out_trade_no'])->withoutGlobalScopes()->first();
+            $order = Order::where('order_no', $message['out_trade_no'])->withoutGlobalScopes()->first();
             \Log::info($order);
         
             if (!$order || $order->pay_time) { // 如果订单不存在 或者 订单已经支付过了
@@ -28,13 +27,13 @@ class FoodPayController extends Controller
         
             if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
                 // 用户是否支付成功
-                if (array_get($message, 'result_code') === 'SUCCESS') {
+                if ($message['result_code'] === 'SUCCESS') {
                     $order->pay_time = data('Y-m-d H:i:s', time()); // 更新支付时间为当前时间
                     $order->trans_no = $message['transaction_id']; // 更新支付时间为当前时间
                     $order->status = OrderStatus::PAID;
         
                 // 用户支付失败
-                } elseif (array_get($message, 'result_code') === 'FAIL') {
+                } elseif ($message['result_code'] === 'FAIL') {
                     $order->status = OrderStatus::UNPAID;
                 }
             } else {
