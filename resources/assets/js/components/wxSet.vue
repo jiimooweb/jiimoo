0ab3441c83778f755cb7431e4ede7843fdb2c2ae
dongfang <1163129165@qq.com>
2018-07-03 11:51:07 +0800

    commit

--- resources/assets/js/components/wxSet.vue
+++ resources/assets/js/components/wxSet.vue
@@ -305,7 +305,7 @@ export default {
                     // this.preViewQrcode = this.convertBase64UrlToBlob(res.data);
                     console.log(res.data);
                     window.URL.revokeObjectURL(this.preViewQrcode)
-                    this.preViewQrcode = window.URL.createObjectURL(res.data);
+                    this.preViewQrcode = window.URL.createObjectURL(res);
                     console.log(this.preViewQrcode);
                     
                 },res=>{
