dcf4cb1756ba6b5439b3bb0b7be659cd097cddfc
FanWaterWater <598804008@qq.com>
2018-07-03 17:59:30 +0800

    commit

--- app/Api/Controllers/Foods/OrderController.php
+++ app/Api/Controllers/Foods/OrderController.php
@@ -287,7 +287,7 @@ class OrderController extends Controller
             'touser' => $order->fan->openid,
             'template_id' => $template_id,
             'page' => '/pages/index/index',
-            'form_id' => 'wx02212940840899422628fe1b2674707885',
+            'form_id' => 'wx03144853904135b013ab78822603435514',
             'data' => [
                 'keyword1' => $order->order_no,
                 'keyword2' => '任意门奶茶',
