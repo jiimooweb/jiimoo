abe4fd1269257a70d0e8b436d8512969dd535730
FanWaterWater <598804008@qq.com>
2018-07-03 12:14:18 +0800

    commit

--- app/Api/Controllers/Foods/OrderController.php
+++ app/Api/Controllers/Foods/OrderController.php
@@ -126,9 +126,7 @@ class OrderController extends Controller
         if($setting->offer_status == 1) {
             $offers = json_decode($setting->offer);
             foreach($offers as $offer) {
-                if($price_total >= $offer['condition']) {
-                    $mj_offer = $offer['reduce'];
-                }
+                var_dump($offer);
             }
         }
 
