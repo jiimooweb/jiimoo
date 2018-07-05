5e542c527a5aa733a493e39b685f31e17bf89654
FanWaterWater <598804008@qq.com>
2018-07-05 20:18:39 +0800

    commit

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -38,8 +38,7 @@ class SwooleCommand extends Command
      */
     public function handle()
     {
-        $http = new \swoole_http_server("127.0.0.1", 9501);
-        echo '连接成功';
+        $http = new \swoole_http_server("0.0.0.0", 9501);
 
         $http->on("start", function ($server) {
             echo "Swoole http server is started at http://127.0.0.1:9501\n";
