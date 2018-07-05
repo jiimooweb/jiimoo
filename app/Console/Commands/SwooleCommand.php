21147cff94118cf2551a38ee63abfbf08994b488
FanWaterWater <598804008@qq.com>
2018-07-05 19:50:10 +0800

    commit

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -38,21 +38,17 @@ class SwooleCommand extends Command
      */
     public function handle()
     {
-        $server = new \swoole_websocket_server("0.0.0.0", 9501);
-
-        $server->on('open', function (swoole_websocket_server $server, $request) {
-            echo "server: handshake success with fd{$request->fd}\n";
+        $server = new swoole_server("127.0.0.1", 9503);
+        $server->on('connect', function ($server, $fd){
+            echo "connection open: {$fd}\n";
         });
-
-        $server->on('message', function (swoole_websocket_server $server, $frame) {
-            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-            $server->push($frame->fd, "this is server");
+        $server->on('receive', function ($server, $fd, $reactor_id, $data) {
+            $server->send($fd, "Swoole: {$data}");
+            $server->close($fd);
         });
-
-        $server->on('close', function ($ser, $fd) {
-            echo "client {$fd} closed\n";
+        $server->on('close', function ($server, $fd) {
+            echo "connection close: {$fd}\n";
         });
-
         $server->start();
     }
 }
