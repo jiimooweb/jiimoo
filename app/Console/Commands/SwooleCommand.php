86f115f7b5b7cf68426837dc076588496e42f5fe
FanWaterWater <598804008@qq.com>
2018-07-06 16:51:16 +0800

    commit

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -42,8 +42,8 @@ class SwooleCommand extends Command
 
         $server->set([
                 'work_num'=>1,
-                'ssl_cert_file'=> '/usr/local/nginx/conf/ssl/1527670808768.crt',
-                'ssl_key_file' => '/usr/local/nginx/conf/ssl/1527670808768.key'
+                'ssl_cert_file'=> '/usr/local/nginx/conf/ssl/ssl.crt',
+                'ssl_key_file' => '/usr/local/nginx/conf/ssl/ssl.key'
             ]);
 
         $server->on('open', function (\swoole_websocket_server $server, $request) {
