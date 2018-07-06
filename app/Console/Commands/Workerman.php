5278c43065f71f4d539a8410661ce434662d1524
FanWaterWater <598804008@qq.com>
2018-07-07 00:33:59 +0800

    commit

--- app/Console/Commands/Workerman.php
+++ app/Console/Commands/Workerman.php
@@ -58,29 +58,25 @@ class Workerman extends Command
         global $argv;
         $arg = $this->argument('action');
         $argv [1] = $arg;
-        
-        $ws = new Worker("websocket://0.0.0.0:9011");
 
-        $ws->count = 4;
-
-        $ws->onConnect = function($connection)
-        {
-            echo "new connection\n";
+        $context = array(
+            // 更多ssl选项请参考手册 http://php.net/manual/zh/context.ssl.php
+            'ssl' => array(
+                // 请使用绝对路径
+                'local_cert'                 => '/usr/local/nginx/conf/ssl/ssl.crt', // 也可以是crt文件
+                'local_pk'                   => '/usr/local/nginx/conf/ssl/ssl.key',
+                'verify_peer'                => false,
+                // 'allow_self_signed' => true, //如果是自签名证书需要开启此选项
+            )
+        );
+        // 这里设置的是websocket协议（端口任意，但是需要保证没被其它程序占用）
+        $worker = new Worker('websocket://0.0.0.0:443', $context);
+        // 设置transport开启ssl，websocket+ssl即wss
+        $worker->transport = 'ssl';
+        $worker->onMessage = function($con, $msg) {
+            $con->send('ok');
         };
 
-        $ws->onMessage = function($connection, $data)
-        {
-            echo $data."\n";
-            
-            $connection->send('hello1');
-        };
-
-        $ws->onClose = function($connection)
-        {
-            echo "Connection closed\n";
-        };
-
-        // Run worker
         Worker::runAll();
     }
 
