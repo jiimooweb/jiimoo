73067fc2e2ed7733c937bfde4e2d5d4ea2e8366f
dongfang <1163129165@qq.com>
2018-07-06 14:47:39 +0800

    commit

--- resources/assets/js/api/baidumap/baidu-map.js
+++ resources/assets/js/api/baidumap/baidu-map.js
@@ -2,7 +2,7 @@ export default{
     init: function (){
         //console.log("初始化百度地图脚本...");
         const AK = "AK密钥";
-        const BMap_URL = "http://api.map.baidu.com/api?v=2.0&ak=DK5ggqGQ9kcOAriasVEeeZcSisLNr4iZ&s=1&callback=onBMapCallback";
+        const BMap_URL = "http://api.map.baidu.com/api?v=2.0&ak=DK5ggqGQ9kcOAriasVEeeZcSisLNr4iZ&s=1&callback=onBMapCallback&s=1";
         return new Promise((resolve, reject) => {
           // 如果已加载直接返回
           if(typeof BMap !== "undefined") {
