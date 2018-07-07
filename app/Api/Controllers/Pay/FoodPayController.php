834ff161925726f00e16b2dfa6bb48136114aae4
FanWaterWater <598804008@qq.com>
2018-07-07 11:39:40 +0800

    commit

--- app/Api/Controllers/Pay/FoodPayController.php
+++ app/Api/Controllers/Pay/FoodPayController.php
@@ -4,6 +4,7 @@ namespace App\Api\Controllers\Pay;
 
 use App\Utils\OrderStatus;
 use App\Models\Foods\Order;
+use App\Services\WebSocket;
 use App\Services\WechatPay;
 use Illuminate\Http\Request;
 use App\Api\Controllers\Controller;
@@ -12,15 +13,17 @@ class FoodPayController extends Controller
 {
     public function notify()
     {
-        $notify_url = config('notify.wechat.foods') . '/' . request()->xcx_id;
+        $xcx_id = request()->xcx_id;
+
+        $notify_url = config('notify.wechat.foods') . '/' . $xcx_id;
+
         $wechatPay = new WechatPay($notify_url);
         
-        $app = $wechatPay->getApp(request()->xcx_id);
+        $app = $wechatPay->getApp($xcx_id);
 
-        $response = $app->handlePaidNotify(function($message, $fail){
+        $response = $app->handlePaidNotify(function($message, $fail) use ($xcx_id){
 
             $order = Order::where('order_no', $message['out_trade_no'])->withoutGlobalScopes()->first();
-            \Log::info($order);
         
             if (!$order || $order->pay_time) { // 如果订单不存在 或者 订单已经支付过了
                 return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
@@ -32,16 +35,14 @@ class FoodPayController extends Controller
                     $order->pay_time = date('Y-m-d H:i:s', time()); // 更新支付时间为当前时间
                     $order->trans_no = $message['transaction_id']; // 更新支付时间为当前时间
                     $order->status = OrderStatus::PAID;
-        
-                // 用户支付失败
-                } elseif ($message['result_code'] === 'FAIL') {
-                    $order->status = OrderStatus::UNPAID;
+                    $order->save();
+                    WebSocket::sendOrderMsg('xcx_id'.$xcx_id, $order);
                 }
             } else {
                 return $fail('通信失败，请稍后再通知我');
             }
         
-            $order->save(); // 保存订单
+            
         
             return true; // 返回处理完成
         });
--- app/Services/WebSocket.php
+++ app/Services/WebSocket.php
@@ -9,20 +9,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class WebSocket extends Model
 {
-
-
-    public function sendOrderMsg() {
+    public static function sendOrderMsg($uid, $order ,$port = '8150') {
          // 建立socket连接到内部推送端口
-        $client = stream_socket_client('tcp://127.0.0.1:8150', $errno, $errmsg, 1);
+        $client = stream_socket_client("tcp://127.0.0.1:$port", $errno, $errmsg, 1);
         // 推送的数据，包含uid字段，表示是给这个uid推送
-        $data = array('uid'=>'uid1', 'percent'=>'88%');
+        $data = array('uid'=> $uid, 'data'=> $order);
         // 发送数据，注意8150端口是Text协议的端口，Text协议需要在数据末尾加上换行符
         fwrite($client, json_encode($data)."\n");
-        // 读取推送结果å
-        echo fread($client, 8192);
+        // 读取推送结果
+        // echo fread($client, 8192);
+
+        fclose($client);
     }
     
 
-    
-    
 }
