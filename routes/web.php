0db2388dd1e3fef80f461f6e705e9843c0dec800
FanWaterWater <598804008@qq.com>
2018-07-07 11:03:45 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::get('/', function() {
 
 Route::get('socket', function() {
     // 建立socket连接到内部推送端口
-    $client = stream_socket_client('tcp://127.0.0.1:5678', $errno, $errmsg, 1);
+    $client = stream_socket_client('tcp://127.0.0.1:8150', $errno, $errmsg, 1);
     // 推送的数据，包含uid字段，表示是给这个uid推送
     $data = array('uid'=>'uid1', 'percent'=>'88%');
     // 发送数据，注意5678端口是Text协议的端口，Text协议需要在数据末尾加上换行符
