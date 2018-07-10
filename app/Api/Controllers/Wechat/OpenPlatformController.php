741ab09b29b67223c01480c0292a71f83f3e9fd6
FanWaterWater <598804008@qq.com>
2018-07-10 19:59:27 +0800

    commit

--- app/Api/Controllers/Wechat/OpenPlatformController.php
+++ app/Api/Controllers/Wechat/OpenPlatformController.php
@@ -324,9 +324,11 @@ class OpenPlatformController extends Controller
     {
         $path = request()->path;
         $scene = request()->scene;
+        $width = request()->width;
         $miniProgram = OpenPlatform::getMiniProgram(request()->xcx_id);
-        return $miniProgram->app_code->get($path, [
-            'scene' => $scene
+        return $miniProgram->app_code->getUnlimit($scene, [
+            'path' => $path,
+            'width' => $width
         ]);
         // $filename = $response->save('wechat/miniprogram/','minicode.png');
     }
