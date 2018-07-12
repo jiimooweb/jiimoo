1febe95a5c8863cd253188e965bd7b8c8bf6c341
FanWaterWater <598804008@qq.com>
2018-07-12 12:21:28 +0800

    commit

--- app/Services/Map.php
+++ app/Services/Map.php
@@ -5,14 +5,6 @@ namespace App\Services;
 class Map
 {
     private static $EARTH_RADIUS = 6378137;//赤道半径(单位m)
-
-    /**
-	 * 转化为弧度(rad)
-	 * */
-	private static function rad($d)
-	{
-	   return $d * pi() / 180.0;
-	}
 	/**
 	 * 基于googleMap中的算法得到两经纬度之间的距离,计算精度与谷歌地图的距离精度差不多，相差范围在0.2米以下
 	 * @param lon1 第一点的精度
@@ -23,14 +15,14 @@ class Map
 	 * */
 	public static function GetDistance($lon1,$lat1,$lon2,$lat2)
 	{
-        $radLat1 = self::rad($lat1);
-        $radLat2 = self::rad($lat2);
+        $radLat1 = deg2rad($lat1); //deg2rad()函数将角度转换为弧度
+        $radLat2 = deg2rad($lat2);
+        $radLng1 = deg2rad($lng1);
+        $radLng2 = deg2rad($lng2);
         $a = $radLat1 - $radLat2;
-        $b = self::rad($lon1) - self::rad($lon2);
-        $s = 2 * asin(sqrt(pow(sin($a/2),2) + cos($radLat1) * cos($radLat2)* pow(sin($b/2),2)));
-        $s = $s * self::$EARTH_RADIUS;
-	    $s =  round($s * 10000) / 10000;
-	   return $s;
+        $b = $radLng1 - $radLng2;
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137 * 1000;
+        return $s;
 	}
 
     
