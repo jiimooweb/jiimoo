d829c393ceabc4e3aca5a72f1dfca8301d6d806b
FanWaterWater <598804008@qq.com>
2018-07-12 12:22:26 +0800

    commit

--- app/Services/Map.php
+++ app/Services/Map.php
@@ -17,8 +17,8 @@ class Map
 	{
         $radLat1 = deg2rad($lat1); //deg2rad()函数将角度转换为弧度
         $radLat2 = deg2rad($lat2);
-        $radLng1 = deg2rad($lng1);
-        $radLng2 = deg2rad($lng2);
+        $radLng1 = deg2rad($lon1);
+        $radLng2 = deg2rad($lon2);
         $a = $radLat1 - $radLat2;
         $b = $radLng1 - $radLng2;
         $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137 * 1000;
