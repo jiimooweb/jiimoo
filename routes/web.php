da9749dc6500cbea4d096bd46a5b702c60732e20
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-14 15:36:35 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -80,5 +80,21 @@ Route::post('/upload', function () {
     dd($file);
 })->middleware('cors');
 
+Rouer::get('wechat-server', function() {
+    $config = [
+        'app_id' => 'wx8d65b1fa98bbf7d1',
+        'secret' => '492a72af7012e9b5d201176c855c6c7a',
+        'token' => 'rdoorweb',
+        'response_type' => 'array',
+        //...
+    ];
+
+    $app = EasyWeChat\Factory::officialAccount($config);
+
+    $response = $app->server->serve();
+
+    return $response;
+});
+
 
 
