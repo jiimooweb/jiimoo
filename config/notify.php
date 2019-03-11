7ae8c1d0d963275cdf9f9745d7c5341e0581b7d0
jimocc <245629490@qq.com>
2019-03-11 18:46:29 +0800

    投票模块：定时任务

--- config/notify.php
+++ config/notify.php
@@ -2,6 +2,7 @@
 
 return [
     'wechat' => [
-        'foods' => config('app.url'). '/wechat/pay_notify/foods'
+        'foods' => config('app.url'). '/wechat/pay_notify/foods',
+        'reward'=>config('app.url'). '/wechat/pay_notify/reward',
     ],
 ];
